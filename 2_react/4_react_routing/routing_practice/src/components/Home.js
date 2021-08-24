1892bac823ee8266f9ffbace533b54100b8a7385
Fumie <funismo@gmail.com>
2021-08-24 01:03:54 -0700

    second commit

---
+++ 2_react/4_react_routing/routing_practice/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from '@reach/router';
+
+const Home = (props) => {
+
+return (
+    <div style={{textAlign: "center"}}>
+        <h1>Welcome</h1>
+
+        {/* <p><Link to = "/home">Home</Link></p>
+        <p><Link to = "/4">Number 4</Link></p>
+        <p><Link to = "/hello">Hello</Link></p>
+        <p><Link to = "/hello/blue/red">See it in color</Link></p> */}
+    </div>
+);
+}
+
+export default Home;
