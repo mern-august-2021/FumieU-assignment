14be01c7d452ab92d1ad465f1a7142035e8a8146
Fumie <funismo@gmail.com>
2021-08-26 19:47:43 -0700

    second commit

--- 2_react/4_react_routing/routing_practice/src/App.js
+++ 2_react/4_react_routing/routing_practice/src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
 import { Router } from '@reach/router';
 import Home from './components/Home.js';
-import Four from './components/Four.js'
-import Color from './components/Color.js'
-import Hello from './components/Hello.js'
+
 
 function App() {
   return (
     <div className="App">
       <Router>
           <Home path="/home" />
-          <Four path="/4" />
-          <Hello path="/hello" />
-          <Color path="/hello/blue/red" />
+          <Home path="/:word" />
+          <Home path="/:word/:color/:bgColor" />
       </Router>
     </div>
   );
--- 2_react/4_react_routing/routing_practice/src/components/Home.js
+++ 2_react/4_react_routing/routing_practice/src/components/Home.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Link } from '@reach/router';
+
+
 
 const Home = (props) => {
+    if (!props.word){
+        return (
+            <div style={{textAlign: "center"}}>
+                <h1>Welcome!</h1>
+            </div>
+        )
+        }else{
+        if(isNaN(props.word)){
+            return (
+                <div style={{textAlign: "center"}}>
+                    <h1 style ={
+                        props.color
+                        ?{ color: props.color, backgroundColor: props.bgColor }
+                        :null
+                    }>The word is: {props.word}</h1>
 
-return (
-    <div style={{textAlign: "center"}}>
-        <h1>Welcome</h1>
-
-        {/* <p><Link to = "/home">Home</Link></p>
-        <p><Link to = "/4">Number 4</Link></p>
-        <p><Link to = "/hello">Hello</Link></p>
-        <p><Link to = "/hello/blue/red">See it in color</Link></p> */}
-    </div>
-);
-}
+                </div>
+            )
+        }else{
+            return (
+                <div style={{textAlign: "center"}}>
+                    <h1> The number is: {props.word}</h1>
+                </div>
+            )
+        }
+    }
+    }
 
 export default Home;
