939658313ea299e45d8a6e01e4065be2972bca26
Fumie <funismo@gmail.com>
2021-09-05 11:49:43 -0700

    second commit

---
+++ 5_full_stack_mern/product_manager_1/client/src/views/Main.js
@@ -0,0 +1,15 @@
+import React, {useState}  from 'react'
+import Form from '../components/Form.js';
+
+
+const Main = () => {
+
+
+    return (
+        <div style = {{textAlign: "center"}}>
+            <h2>Product Manager</h2>
+            <Form />
+        </div>
+    )
+}
+export default Main;
